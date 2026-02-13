e3eb8fe9906bb846d18f4895f9958bd186747010
adereber <artur.dereberg@sogeti.se>
2026-02-13 12:00:33 +0100

    first commit

---
+++ Learningapp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Learningapp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
